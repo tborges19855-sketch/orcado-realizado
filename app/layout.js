ca071b17e6c6ce54da2d8320b1b023a7da8c6e4f
tborges19855-sketch <tborges19855@gmail.com>
2026-09-22 15:43:32 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Acompanhamento do Orçado x Realizado",
+  description: "Gestão financeira do condomínio"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
